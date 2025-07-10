4739a513a67d2773bd70d5e4c05d0f11176e2520
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-07-11 02:20:07 +0600

    Added Announcement Bar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router"
 import ScrollToHashElement from "..//utils/ScrollToHashElement"
+import Announcement from "../components/ui/Announcement"
 import GoTopBtn from "../components/ui/GoTopBtn"
 import ThemeToggle from "../components/ui/ThemeToggle"
 import ThemeProvider from "../contexts/ThemeContext"
@@ -9,6 +10,7 @@ const MainLayout = () => {
     <ThemeProvider>
       <ScrollToHashElement />
       <main>
+        <Announcement />
         <Outlet />
 
         <section className="fixed bottom-4 right-0 -translate-x-1/2 z-500 flex flex-col items-center space-y-2">
