6a176a68de2e20384c441a11a7a72baf25d03531
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-07-11 19:20:33 +0600

    Added AuthContext, AuthProvider & made hook

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,72 @@
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import { app } from '../firebase/firebase.config';
+// import { toast } from 'react-toastify';
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+export const AuthContext = createContext(null)
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// AuthProvider
+const AuthProvider = ({ children }) => {
+  const auth = getAuth(app)
+  const googleProvider = new GoogleAuthProvider();
+  // const notifySuccess = () => toast.success(<ToastSuccess />);
+  // const notifyFailed = (error) => toast.error(<ToastFailed error={error} />);
+  // const ToastSuccess = () => (
+  //   <span className='text-lg text-green-600 font-semibold font-poppins'>Profile updated!</span>
+  // );
+  // const ToastFailed = ({ error }) => (
+  //   <div className='font-semibold font-poppins'>
+  //     <div className='flex gap-3 mb-1'>
+  //       <span className='text-lg text-red-600 font-semibold font-poppins'>Profile didn't update!</span>
+  //     </div>
+  //     <p>{error}</p>
+  //   </div>
+  // );
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider)
+  }
+
+  const logOut = () => { setLoading(true); return signOut(auth) }
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    })
+  }
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      if (currentUser) { setUser(currentUser); setLoading(false); }
+      else { setUser(null); setLoading(false); }
+    });
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = { user, setUser, loading, setLoading, createUser, signIn, signInWithGoogle, logOut, updateUserProfile };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import { ToastContainer } from 'react-toastify'
+import AuthProvider from './contexts/AuthProvider'
 import './index.css'
 import { router } from './routes/router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer autoClose={3500} theme="light"
+        closeOnClick pauseOnFocusLoss draggable pauseOnHover='true' />
+    </AuthProvider>
   </StrictMode>
 )
