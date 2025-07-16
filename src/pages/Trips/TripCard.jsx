8be948b3c438e2df2a11d72d372b4010404d9826
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-07-16 08:49:45 +0600

    Added Trips Page

---
+++ src/pages/Trips/TripCard.jsx
@@ -0,0 +1,59 @@
+import { motion } from 'framer-motion';
+import { FaLocationArrow } from "react-icons/fa";
+import { TbListDetails } from "react-icons/tb";
+import { useNavigate } from 'react-router';
+
+const TripCard = ({ trip }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="rounded-3xl overflow-hidden transition duration-300 group bg-white dark:bg-zinc-900 border border-border shadow-sm hover:shadow-md">
+      <div className="relative">
+        <img
+          src={trip.images?.[0] || "/fallback.jpg"}
+          alt={trip.title}
+          className="h-48 sm:h-52 md:h-56 w-full rounded-3xl object-cover group-hover:scale-105 transition-transform duration-300"
+        />
+      </div>
+
+      <div className="p-4 space-y-4">
+        <div className="flex justify-between items-center font-bricolage-grotesque">
+          <h3 className="text-lg sm:text-xl md:text-2xl font-semibold text-text truncate">
+            {trip.title}
+          </h3>
+          <p className="font-semibold text-text text-base sm:text-lg">
+            ৳{trip.price}
+            <span className="text-sm font-normal">/trip</span>
+          </p>
+        </div>
+
+        <div className="flex items-center justify-between gap-2 flex-wrap">
+          <p className="text-sm sm:text-base text-text-muted font-medium flex items-center gap-1 truncate">
+            <span className="group-hover:text-emerald-500 transition-colors duration-300">
+              <FaLocationArrow size={16} />
+            </span>
+            {trip.location}
+          </p>
+
+          <p className="w-fit text-xs sm:text-sm font-medium font-bricolage-grotesque bg-text-muted/10 text-text-muted group-hover:bg-emerald-500/10 group-hover:text-emerald-500 px-3 py-1 rounded-full transition-colors duration-300">
+            {trip.tourType}
+          </p>
+        </div>
+
+        <div className="pt-4">
+          <motion.button
+            whileHover={{ scale: 1.03 }}
+            whileTap={{ scale: 0.97 }}
+            onClick={() => navigate(`/packages/${trip._id}`)}
+            className="w-full flex items-center justify-center gap-2 rounded-full shadow-md transition duration-200 bg-gray-900 text-white hover:bg-gray-800 dark:bg-white dark:text-gray-900 dark:hover:bg-gray-200 px-5 py-2 text-sm sm:text-base font-medium"
+          >
+            <TbListDetails size={20} />
+            View Details
+          </motion.button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TripCard;
--- src/pages/Trips/Trips.jsx
+++ src/pages/Trips/Trips.jsx
@@ -1,6 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import LoadingHash from "../../components/shared/LoadingHash";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import TripCard from "./TripCard";
+
 const Trips = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user, loading } = useAuth();
+  const email = user?.email;
+
+  const { data: packages = [], isLoading, refetch } = useQuery({
+    queryKey: ["stories", email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/packages`);
+      return res.data;
+    },
+    enabled: !!email,
+  });
+
+  if (loading || isLoading) return <LoadingHash />;
+  refetch();
+
+
   return (
-    <div>trips</div>
-  )
-}
-export default Trips
+    <div className="max-w-[1400px] mx-auto px-8 xs:px-16 sm:px-10 xl:px-12 py-12">
+      <div className={`flex flex-col gap-6 sm:flex-row sm:flex-wrap sm:justify-center md:grid md:grid-cols-2 lg:grid-cols-3`}>
+        {packages.map((trip) => (
+          <TripCard key={trip._id} trip={trip} />
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Trips;
